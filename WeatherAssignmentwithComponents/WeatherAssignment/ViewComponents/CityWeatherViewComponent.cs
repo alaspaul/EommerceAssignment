04830044a9fbbbfe8c42b7c1c855e2062059aece
alaspaul <53499601+alaspaul@users.noreply.github.com>
2024-09-02 21:55:28 +0800

    Add files via upload

---
+++ WeatherAssignmentwithComponents/WeatherAssignment/ViewComponents/CityWeatherViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherAssignment.Models;
+namespace WeatherAssignament.ViewComponents
+{
+    public class CityWeatherViewComponent :ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(CityWeather city)
+        {
+            ViewBag.BGColor = BackgroundColor(city.TemperatureFahrenheit);
+
+            return View(city);
+        }
+
+
+        public string BackgroundColor(int fahrenheit)
+        {
+            if (fahrenheit < 44)
+            {
+                return "blue-back";
+            }
+            else if (fahrenheit > 44 && fahrenheit < 74)
+            {
+                return "green-back";
+            }
+            else
+            {
+                return "orange-back";
+            }
+        }
+    }
+}
