a5b26950b17b91ff42c93d01ae25f7afc718e0b1
alaspaul <53499601+alaspaul@users.noreply.github.com>
2024-09-04 22:06:46 +0800

    Add files via upload

---
+++ SocialMediaAssignment/SocialMediaAssignment/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using SocialMediaAssignment;
+
+namespace ConfigurationDemoSolution.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly SocialMediaLinkOptions _socialMediaLinksOptions;
+
+        public HomeController(IOptions<SocialMediaLinkOptions> socialMediaLinksOptions)
+        {
+            _socialMediaLinksOptions = socialMediaLinksOptions.Value;
+        }
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            ViewBag.SocialMediaLinks = _socialMediaLinksOptions;
+            return View();
+        }
+    }
+}
---
+++ SocialMediaAssignment/SocialMediaAssignment/SocialMediaLinkOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace SocialMediaAssignment
+{
+    public class SocialMediaLinkOptions
+    {
+        public string? Facebook {  get; set; }
+
+        public string? Instagram { get; set; }
+
+        public string? Twitter { get; set; }
+
+        public string? Youtube { get; set; }
+    }
+}
