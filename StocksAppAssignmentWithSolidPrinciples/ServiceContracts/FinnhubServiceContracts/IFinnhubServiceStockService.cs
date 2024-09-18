4ca648c600d4561d4fb1422fb2140eb55c5405f6
paul irvin alas <paul4145irvin@gmail.com>
2024-09-18 19:15:11 +0800

    added stocks app with solid principles

---
+++ StocksAppAssignmentWithSolidPrinciples/ServiceContracts/FinnhubServiceContracts/IFinnhubServiceStockService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceContracts
+{
+    /// <summary>
+    /// Represents a service that makes HTTP requests to finnhub.io
+    /// </summary>
+    public interface IFinnhubServiceStockService
+    {
+
+
+        /// <summary>
+        /// Returns list of all stocks supported by an exchange (default: US)
+        /// </summary>
+        /// <returns>List of stocks</returns>
+        Task<List<Dictionary<string, string>>?> GetStocks();
+
+    }
+}
+
---
+++ StocksAppAssignmentWithSolidPrinciples/ServiceContracts/StocksServiceContracts/ISellOrderService.cs
@@ -0,0 +1,24 @@
+﻿using ServiceContracts.DTO.BuyOrderDTO;
+using ServiceContracts.DTO.SellOrderDTO;
+
+namespace ServiceContracts.StocksServiceContracts
+{
+    public interface ISellOrderService
+    {
+
+        /// <summary>
+        /// creates a new sell order
+        /// </summary>
+        /// <param name="request">Accepts an object type of SellOrderRequest</param>
+        /// <returns>Returns The newly created SellORder as a SellOrderResponse</returns>
+        public Task<SellOrderResponse> CreateSellOrder(SellOrderRequest request);
+
+        /// <summary>
+        /// Gets The List of Sell Orders
+        /// </summary>
+        /// <returns>returns a List of SellOrderResponse</returns>
+        public Task<List<SellOrderResponse>> GetSellOrders();
+
+
+    }
+}
